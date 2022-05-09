62125f5f17ae9657224ec7646e8824393aafaddf
xk999 <xeniak91@yandex.ru>
2022-05-09 15:49:49 +0500

    add langdetect (issue #1)

--- alib.py
+++ alib.py
@@ -4,22 +4,26 @@ import requests
 from urllib import request
 from bs4 import BeautifulSoup
 import time
+from langdetect import detect
 
 wb = openpyxl.load_workbook(filename="books.xlsx")
 sheet = wb.active
 rows = sheet.max_row
 
+num = int(input("Which row shall we start with? "))
 list1=[]
 #concatenate author and title
-for i in range(2,rows+1):
+for i in range(num,rows+1):
     title = (sheet['A'+str(i)].value,sheet['B'+str(i)].value)
     list1.append(title)    
 
 book_list = []
-#convert all rows to the required encoding
+#convert rows to the required encoding
 for i in list1:
-    encoded = request.quote(str(i).encode('cp1251'))
-    book_list.append(encoded)
+    lang = detect(str(i))
+    if lang == 'ru' or 'bg': 
+        encoded = request.quote(str(i).encode('cp1251'))
+        book_list.append(encoded)
 
 #search for books
 for book in book_list:
